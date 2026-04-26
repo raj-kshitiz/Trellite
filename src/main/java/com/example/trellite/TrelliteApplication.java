9be6906c6d54ceecf6e50c1ec9cbfa987ea5c624
kshitiz <kshitizrajofficial78@gmail.com>
2026-04-26 14:47:47 +0530

    project created

---
+++ src/main/java/com/example/trellite/TrelliteApplication.java
@@ -0,0 +1,13 @@
+package com.example.trellite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelliteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelliteApplication.class, args);
+    }
+
+}
